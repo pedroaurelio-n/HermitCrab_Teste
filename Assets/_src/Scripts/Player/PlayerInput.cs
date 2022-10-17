8348e85fb043f60ed00155e1778c7fffa0168782
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2022-10-16 23:11:37 -0300

    Ajustes nos elementos de UI. Mudança de input para toques em dispositivos mobile

--- Assets/_src/Scripts/Player/PlayerInput.cs
+++ Assets/_src/Scripts/Player/PlayerInput.cs
@@ -11,14 +11,40 @@ namespace PedroAurelio.HermitCrab
         private RunnerMovement _movement;
         private ShootBullet _shoot;
 
+        private float _screenHalfWidth;
+
         private void Awake()
         {
             _movement = GetComponent<RunnerMovement>();
             _shoot = GetComponent<ShootBullet>();
+
+            _screenHalfWidth = Screen.width * 0.5f;
         }
 
         private void Update()
         {
+            if (Input.touchCount > 0)
+            {
+                foreach (Touch touch in Input.touches)
+                {
+                    if (touch.position.x <= _screenHalfWidth)
+                    {
+                        if (touch.phase == TouchPhase.Began)
+                            _movement.SetJumpInput(true);
+                        else if (touch.phase == TouchPhase.Ended)
+                            _movement.SetJumpInput(false);
+                    }
+                    else
+                    {
+                        if (touch.phase == TouchPhase.Began)
+                            _shoot.SetShootInput(true);
+                        else if (touch.phase == TouchPhase.Ended)
+                            _shoot.SetShootInput(false);
+                    }
+                }
+            }
+
+            #if UNITY_EDITOR
             if (Input.GetMouseButtonDown(0))
             {
                 _shoot.SetShootInput(true);
@@ -38,6 +64,7 @@ namespace PedroAurelio.HermitCrab
             {
                 _movement.SetJumpInput(false);
             }
+            #endif
         }
     }
 }
